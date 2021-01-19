3e0880248cb512ba75716380c70a079fc33445b7
Salas <radjefsls@gmail.com>
2021-01-19 12:41:32 +0100

    commit

---
+++ app/src/main/java/com/loki/lokiapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.loki.lokiapp;
+
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    MediaPlayer mediaPlayer;
+
+
+
+
+    public void select(View view){
+        textView = (TextView) findViewById(R.id.textView);
+        mediaPlayer = MediaPlayer.create(this,R.raw.audio);
+        int time = 1200;
+        int ms = time * 1000;
+        new CountDownTimer(ms, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long nb_m = (((millisUntilFinished / 1000) % 86400 ) % 3600 ) / 60;
+                long nb_s = (((millisUntilFinished / 1000) % 86400 ) % 3600 ) % 60  ;
+
+                textView.setText(String.valueOf(nb_m) + ":" + String.valueOf(nb_s));
+            }
+
+            @Override
+            public void onFinish() {
+                textView.setText("Le repas de loki est pret !");
+                mediaPlayer.start();
+
+            }
+        }.start();
+
+    }
+
+    public void stop(View view){
+        mediaPlayer.stop();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
